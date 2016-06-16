3c2f70a0bc99e1e1d57991320cc835b60f370a0a
Victoria <victoria.hristova@dock9.co.uk>
2016-06-16 09:19:52 +0100

    Added code files + license

---
+++ CustomWidgets/DownloadList/CustomDownloadListView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Telerik.Sitefinity.Modules.Libraries.Documents;
+using Telerik.Sitefinity.Modules.Libraries.Web.UI.Documents;
+
+namespace SitefinityWebApp.CustomWidgets.DownloadList
+{
+    public class CustomDownloadListView : DownloadListView
+    {
+        public override string MasterViewName
+        {
+            get
+            {
+                return "CustomMasterListView";
+            }
+            set
+            {
+                base.MasterViewName = value;
+            }
+        }
+    }
+}
